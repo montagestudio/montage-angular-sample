542ba52adcfa88749b78842fd47ab3fce44df397
Harold Thetiot <hthetiot@gmail.com>
2017-06-28 19:27:40 -0700

    load montage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Load Montage
+import { Montage } from 'montage';
+console.log(Montage)
+
 import { AppComponent } from './app.component';
 
 @NgModule({
